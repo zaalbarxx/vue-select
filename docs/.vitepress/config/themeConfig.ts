8c3a1b107b3282cc9ce11caffb246500b5e597e4
Jeff Sagal <sagalbot@gmail.com>
2022-07-15 09:57:37 -0700

    bump to vitepress 1.0.0-alpha.4

--- docs/.vitepress/config/themeConfig.ts
+++ docs/.vitepress/config/themeConfig.ts
@@ -1,77 +1,75 @@
-import type { DefaultTheme } from 'vitepress'
+import { defineConfig } from 'vitepress'
 
-export const themeConfig: DefaultTheme.Config = {
-    repo: 'sagalbot/vue-select',
-    editLinks: true,
-    docsDir: 'docs',
-    nav: [{ text: 'Sandbox', link: '/sandbox' }],
-    sidebar: [
-        {
-            text: 'Community',
-            collapsable: false,
-            children: [
-                { text: 'Sponsors 🎉', link: 'sponsors' },
-                { text: 'Contributors', link: 'contributors' },
-            ],
+export const themeConfig = defineConfig({
+    themeConfig: {
+        siteTitle: 'Vue Select',
+        // nav: [{link: '/', text: 'Home'}],
+
+        editLink: {
+            pattern:
+                'https://github.com/sagalbot/vue-select/edit/master/docs/:path',
         },
-        {
-            text: 'Getting Started',
-            collapsable: false,
-            children: [
-                { link: 'guide/install', text: 'Installation' },
-                { link: 'guide/options', text: 'Dropdown Options' },
-                { link: 'guide/values', text: 'Selecting Values' },
-                { link: 'guide/upgrading', text: 'Upgrading 2.x to 3.x' },
-            ],
-        },
-        {
-            text: 'Templating & Styling',
-            collapsable: false,
-            children: [
-                { link: 'guide/components', text: 'Child Components' },
-                { link: 'guide/css', text: 'CSS & Selectors' },
-                { link: 'guide/slots', text: 'Slots' },
-            ],
-        },
-        {
-            text: 'Accessibility',
-            collapsable: false,
-            children: [
-                { link: 'guide/accessibility', text: 'WAI-ARIA Spec' },
-                { link: 'guide/localization', text: 'Localization' },
-            ],
-        },
-        {
-            text: 'Use Cases',
-            collapsable: false,
-            children: [
-                { link: 'guide/validation', text: 'Validation' },
-                { link: 'guide/selectable', text: 'Limiting Selections' },
-                { link: 'guide/pagination', text: 'Pagination' },
-                { link: 'guide/infinite-scroll', text: 'Infinite Scroll' },
-                { link: 'guide/vuex', text: 'Vuex' },
-                { link: 'guide/ajax', text: 'AJAX' },
-                { link: 'guide/loops', text: 'Using in Loops' },
-            ],
-        },
-        {
-            text: 'Customizing',
-            collapsable: false,
-            children: [
-                { link: 'guide/keydown', text: 'Keydown Events' },
-                { link: 'guide/positioning', text: 'Dropdown Position' },
-                { link: 'guide/opening', text: 'Dropdown Opening' },
-                { link: 'guide/filtering', text: 'Option Filtering' },
-            ],
-        },
-        {
-            text: 'API',
-            collapsable: false,
-            children: [
-                { link: 'api/props', text: 'Props' },
-                { link: 'api/slots', text: 'Slots' },
-                { link: 'api/events', text: 'Events' },
-            ],
-        },
-    ],
-}
+        sidebar: [
+            {
+                text: 'Community',
+                items: [
+                    { text: 'Sponsors 🎉', link: 'sponsors' },
+                    { text: 'Contributors', link: 'contributors' },
+                ],
+            },
+            {
+                text: 'Getting Started',
+                items: [
+                    { link: 'guide/install', text: 'Installation' },
+                    { link: 'guide/options', text: 'Dropdown Options' },
+                    { link: 'guide/values', text: 'Selecting Values' },
+                    { link: 'guide/upgrading', text: 'Upgrading 2.x to 3.x' },
+                ],
+            },
+            {
+                text: 'Templating & Styling',
+                items: [
+                    { link: 'guide/components', text: 'Child Components' },
+                    { link: 'guide/css', text: 'CSS & Selectors' },
+                    { link: 'guide/slots', text: 'Slots' },
+                ],
+            },
+            {
+                text: 'Accessibility',
+                items: [
+                    { link: 'guide/accessibility', text: 'WAI-ARIA Spec' },
+                    { link: 'guide/localization', text: 'Localization' },
+                ],
+            },
+            {
+                text: 'Use Cases',
+                items: [
+                    { link: 'guide/validation', text: 'Validation' },
+                    { link: 'guide/selectable', text: 'Limiting Selections' },
+                    { link: 'guide/pagination', text: 'Pagination' },
+                    { link: 'guide/infinite-scroll', text: 'Infinite Scroll' },
+                    { link: 'guide/vuex', text: 'Vuex' },
+                    { link: 'guide/ajax', text: 'AJAX' },
+                    { link: 'guide/loops', text: 'Using in Loops' },
+                ],
+            },
+            {
+                text: 'Customizing',
+                items: [
+                    { link: 'guide/keydown', text: 'Keydown Events' },
+                    { link: 'guide/positioning', text: 'Dropdown Position' },
+                    { link: 'guide/opening', text: 'Dropdown Opening' },
+                    { link: 'guide/filtering', text: 'Option Filtering' },
+                ],
+            },
+            {
+                text: 'API',
+                items: [
+                    { link: 'api/props', text: 'Props' },
+                    { link: 'api/slots', text: 'Slots' },
+                    { link: 'api/events', text: 'Events' },
+                ],
+            },
+        ],
+    },
+})
