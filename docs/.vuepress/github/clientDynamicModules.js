ffd544f101a45854e400698712148dd77a58e201
Jeff Sagal <sagalbot@gmail.com>
2022-12-17 12:20:32 -0800

    build(1733): parse deploy preview env int (#1734)

--- docs/.vuepress/github/clientDynamicModules.js
+++ docs/.vuepress/github/clientDynamicModules.js
@@ -31,10 +31,15 @@ async function getSponsors() {
    * Deploy previews don't have access to secrets.
    * Return early since we don't have a token.
    */
-  if (process.env['DEPLOY_PREVIEW']) {
+  if (
+    process.env['DEPLOY_PREVIEW'] &&
+    parseInt(process.env['DEPLOY_PREVIEW'])
+  ) {
     console.log('Skipping sponsors because this is a deploy preview.')
     return []
   }
+
+  console.log('Fetching sponsors...')
 
   const query = `
     {
